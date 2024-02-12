a3e91f92da5c66dad4001a262410c40ca49759f2
Ashar Rasheed <Asharrasheed32@gmail.com>
2024-02-12 18:14:09 +0500

    Add project files.

---
+++ MuhaddisWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MuhaddisWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
